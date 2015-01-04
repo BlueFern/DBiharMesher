1b12f4006278449b37552894d9f556a786a60045
Constantine Zakkaroff <constantine.zakkaroff@canterbury.ac.nz>
2015-01-05 10:37:38 +1300

    Add Another Test for WrapDBihar Filter
    This test works with test input patch boundaries saved in files.

---
+++ test/testWrapDbihar7.cxx
@@ -0,0 +1,77 @@
+#include <sstream>
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkCellArray.h>
+#include <vtkIdList.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkAppendPoints.h>
+
+#include "showPolyData.h"
+
+#include "vtkDbiharPatchFilter.h"
+
+#include "wrapDbiharConfig.h"
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >( ( std::ostringstream() << std::dec << x ) ).str()
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "Starting " << __FILE__ << std::endl;
+
+	vtkSmartPointer<vtkAppendPoints> appendFilter = vtkSmartPointer<vtkAppendPoints>::New();
+
+	for(int i = 0; i < 3; i++)
+	{
+		vtkSmartPointer<vtkXMLPolyDataReader> patchReader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+		patchReader->SetFileName(SSTR(std::string(TEST_DATA_DIR) + "/inputPatch_" << i << ".vtp").c_str());
+		patchReader->Update();
+
+		vtkSmartPointer<vtkDbiharPatchFilter> patchFilter = vtkSmartPointer<vtkDbiharPatchFilter>::New();
+
+		// Set the bounds of the UV space.
+		patchFilter->SetA(0.0);
+		patchFilter->SetB(0.7);
+		patchFilter->SetC(0.0);
+		patchFilter->SetD(1.2);
+
+		// Set the dimensions conditions.
+		patchFilter->SetMQuads(28);
+		patchFilter->SetNQuads(88);
+
+		// Set solution method.
+		patchFilter->SetIFlag(4);
+
+		patchFilter->SetInputData(patchReader->GetOutput());
+		patchFilter->Update();
+
+		appendFilter->AddInputData(patchFilter->GetOutput());
+	}
+
+	appendFilter->Update();
+
+	vtkSmartPointer<vtkPolyData> outputPoints = appendFilter->GetOutput();
+	vtkSmartPointer<vtkIdList> vertices = vtkSmartPointer<vtkIdList>::New();
+	for(int i = 0; i < outputPoints->GetNumberOfPoints(); i++)
+	{
+		vertices->InsertNextId(i);
+	}
+	vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
+	cells->InsertNextCell(vertices);
+	outputPoints->SetVerts(cells);
+
+#if 0
+	vtkSmartPointer<vtkXMLPolyDataWriter> outputWriter = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+	outputWriter->SetFileName("outputPatchPointsCombined.vtp");
+	outputWriter->SetInputData(appendFilter->GetOutput());
+	outputWriter->Update();
+#endif
+
+	showPolyData1(outputPoints);
+
+	std::cout << "Exiting " << __FILE__ << std::endl;
+
+	return EXIT_SUCCESS;
+}
+
