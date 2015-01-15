f24c79d5795b08f9443a10cf9a27e747c6977c7e
Stewart Dowding <sdo1458@gmail.com>
2015-01-15 13:52:56 +1300

    Add Filter To Build Skip Segments
    Created a filter that builds a skip segment from a specified start
    or end point moving in the opposite direction. This segment is a mesh
    of triangles using newly created points and a line that is the last
    ring.

---
+++ src/vtkSkipSegmentFilter.h
@@ -0,0 +1,41 @@
+
+#ifndef __vtkSkipSegmentFilter_h
+#define __vtkSkipSegmentFilter_h
+
+#include <vtkAlgorithm.h>
+#include <vtkPolyDataAlgorithm.h>
+
+class vtkSkipSegmentFilter : public vtkPolyDataAlgorithm
+{
+public:
+	vtkTypeMacro(vtkSkipSegmentFilter,vtkPolyDataAlgorithm);
+	void PrintSelf(ostream& os, vtkIndent indent);
+
+	static vtkSkipSegmentFilter *New();
+
+	vtkSetMacro(SkipSize, int);
+	vtkSetMacro(NumberOfRadialQuads, int);
+	vtkSetMacro(Inlet, bool);
+	vtkSetMacro(Outlet, bool);
+	vtkSetMacro(PointId, vtkIdType);
+
+	static const char *RADII_ARR_NAME;
+
+protected:
+	vtkSkipSegmentFilter();
+	~vtkSkipSegmentFilter() {};
+
+	int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+
+private:
+	vtkSkipSegmentFilter(const vtkSkipSegmentFilter&); // Not implemented.
+	void operator=(const vtkSkipSegmentFilter&); // Not implemented.
+	int NumberOfRadialQuads;
+	int SkipSize;
+	bool Inlet;
+	bool Outlet;
+	vtkIdType PointId;
+
+};
+
+#endif
---
+++ test/testSkipSegmentFilter1.cxx
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkGenericDataObjectReader.h>
+#include <vtkGenericDataObjectWriter.h>
+#include <vtkPolyDataWriter.h>
+
+#include "wrapDbiharConfig.h"
+
+#include "vtkScalarRadiiToVectorsFilter.h"
+#include "vtkSkipSegmentFilter.h"
+#include "showPolyData.h"
+
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "Starting " << __FILE__ << std::endl;
+
+	vtkSmartPointer<vtkGenericDataObjectReader> vesselCentrelineReader = vtkSmartPointer<vtkGenericDataObjectReader>::New();
+	vesselCentrelineReader->SetFileName((std::string(TEST_DATA_DIR) + "/721A_CentrelineResampled_4ECs.vtk").c_str());
+	vesselCentrelineReader->Update();
+
+	vtkPolyData *vesselCentreline = vtkPolyData::SafeDownCast(vesselCentrelineReader->GetOutput());
+
+	vtkSmartPointer<vtkScalarRadiiToVectorsFilter> scalarRadiiToVectorsFilter = vtkSmartPointer<vtkScalarRadiiToVectorsFilter>::New();
+	scalarRadiiToVectorsFilter->SetInputData(vesselCentreline);
+	scalarRadiiToVectorsFilter->Update();
+
+	vtkSmartPointer<vtkSkipSegmentFilter> skipSegmentFilter = vtkSmartPointer<vtkSkipSegmentFilter>::New();
+	skipSegmentFilter->SetInputData(scalarRadiiToVectorsFilter->GetOutput());
+	skipSegmentFilter->SetInlet(true);
+	//skipSegmentFilter->SetOutlet(true);
+	skipSegmentFilter->SetSkipSize(16);
+	skipSegmentFilter->SetPointId(0);
+	//skipSegmentFilter->SetPointId(504);
+	//skipSegmentFilter->SetPointId(658);
+	skipSegmentFilter->SetNumberOfRadialQuads(28);
+	skipSegmentFilter->Update();
+
+	vtkSmartPointer<vtkPolyData> centrelineSegments = skipSegmentFilter->GetOutput();
+	vtkSmartPointer<vtkPolyDataWriter> writer = vtkSmartPointer<vtkPolyDataWriter>::New();
+	writer->SetInputData(centrelineSegments);
+	writer->SetFileName("TEST_721_skip_inlet.vtk");
+	//writer->SetFileName("TEST_721_skip_outlet1.vtk");
+	//writer->SetFileName("TEST_721_skip_outlet2.vtk");
+	writer->SetFileTypeToASCII();
+	writer->Write();
+	skipSegmentFilter->Print(std::cout);
+
+
+
+	return EXIT_SUCCESS;
+}
