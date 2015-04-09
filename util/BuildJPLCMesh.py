72c35f54e50c891621c1616cb5c310990f64e042
Constantine Zakkaroff <constantine.zakkaroff@canterbury.ac.nz>
2015-04-10 10:06:18 +1200

    Complete the code for generating JPLC maps

--- util/BuildJPLCMesh.py
+++ util/BuildJPLCMesh.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr  9 14:38:16 2015
-
-Create an initial JPLC Profile for an ECs Mesh.
+Create an initial JPLC Profile for an ECs Mesh and write it out as .vtp.
 """
 
+# TODO: Separating the dataset into branches would make the script simpler.
+# Branch labelling is also required for processing tree datasets of arbitrary complexity.
+
 import os
-import sys
 import vtk
+import numpy
+import matplotlib.pyplot as pyplot
 
-numBranches = 3
-numQuads = 72
-numECsPerCol = 4
-
+# This is for 216 mesh.
 meshFile = "quadMeshFullECc216.vtp"
+numBranches = 3
+numQuads = 216
+numECsPerCol = 4
+jplcGradient = 0.15
 
-# TODO: To make this script to be able to process meshes of arbitrary tree
-# complexity, the script needs the following:
-# 1. Read command line parameter for the mesh.
-# 2. Mesh needs to have its branches labelled with unique itentifiers.
+jplcMin = 0.2
+jplcMax = 2.7
+
+# Sigmoid function for providing JPLC values. The jplcGradient variable
+# controls the "spread" of the values across the given domain.
+def sigmoidJPLC(x):
+    return jplcMin + (jplcMax / (1.0 + numpy.exp(-jplcGradient * x)))
 
 def main():
+    # While in devel and debug mode.
+    os.chdir("/home/constantine/BlueFern/DbiharPatchFilter/tmpData/c216")
     # Report our CWD just for testing purposes.
-    print "Current working directory:", os.getcwd()    
+    print "CWD:", os.getcwd()    
 
     # Read in the mesh.
     meshReader = vtk.vtkXMLPolyDataReader()
@@ -31,19 +39,6 @@ def main():
 
     mesh = meshReader.GetOutput()
     
-    gridCoords = mesh.GetCellData().GetArray("gridCoords")
-    
-    # This is the part that will have to change, when the branches are labelled.
-    
-    
-    # Iterate over all quad cells in the mesh and pull out the first component of the
-    # "gridCoords" cell data array.
-    # Here we assuming the mesh contains only quad cells.
-    for cellId in range(0, mesh.GetNumberOfCells()):
-        coords = gridCoords.GetTuple(cellId)
-        print coords[0]
-    
-
     # Put it through centroids filter.
     # Use VTK centroid filter to get the centroids in the right order
     # from the reorderedECMeshBranch.
@@ -57,26 +52,70 @@ def main():
     #pointsToVerticesFilter.SetInput(centroidFilter.GetOutput())
     pointsToVerticesFilter.Update()
     
-    centroidsAndVertices = pointsToVerticesFilter.GetOutput()
+    jplcDataset = pointsToVerticesFilter.GetOutput()
     
+    # Here we are assuming that cell ordering has been preserved.
+    gridCoords = mesh.GetCellData().GetArray("gridCoords")
 
+    # Create a new cell data array "parametricDistance" in the centroids
+    # dataset with the values from the previous step. This array is really not
+    # needed for any computations is good to have for verification. Also,
+    # remember the max value for each branch.
+    axialDist = vtk.vtkIntArray()
+    axialDist.SetName("parametriDistance")
     
-    
-    
-    verticesArray = centroidsAndVertices.GetVerts()
-    verticesArray.InitTraversal()
-    
+    # Iterate over all quad cells in the mesh and pull out the first component
+    # of the "gridCoords" cell data array. Here we assuming the mesh contains
+    # only quad cells.
+    # If the branches were labelled properly here, it would be much simpler
+    # to set the neganive axiial distance values by examining the range first.
+    for cellId in range(0, mesh.GetNumberOfCells()):
+        axialDist.InsertNextValue(gridCoords.GetTuple(cellId)[0])
 
-# Create a new cell data array "parametricDistance" in the centroids dataset
-# with the values from the previous step. This array is really not needed for
-# any computations is good to have for verification. Also, remember the max
-# value for each branch.
-
-# Iterate over the "parametricDistance" cell array and for each value call the
-# agonistValue function and store the computed values in the new "InitialJPLC"
-# cell data array.    
+    jplcArray = vtk.vtkDoubleArray()
+    jplcArray.SetName("initialJPLC")
     
+    axialDistRange = [0, 0]
+    axialDist.GetRange(axialDistRange, 0)
+    
+    # Iterate over the "axialDist" cell array and for each value call
+    # the agonistValue function and store the computed values in the new
+    # "initialJPLC" cell data array.
+    for cellId in range(0, axialDist.GetNumberOfTuples()):
+        # Figure out the current branch id.
+        branchId = cellId / (axialDist.GetNumberOfTuples() / 3)
+        
+        # For the first branch the parametric distance value must be in the
+        # range [maxDist - 1, 0). Range values are required for this.
+        if branchId == 0:
+            distVal = axialDist.GetValue(cellId) - axialDistRange[1] - 1
+            jplcVal = sigmoidJPLC(distVal)
+            jplcArray.InsertNextValue(jplcVal)
+        else:
+            distVal = axialDist.GetValue(cellId)
+            jplcVal = sigmoidJPLC(distVal)
+            jplcArray.InsertNextValue(jplcVal)
 
+    # Assert the number of cells is equal to the number of items in the cell arrays.
+    assert axialDist.GetNumberOfTuples() == jplcDataset.GetNumberOfCells(), "Number of cells (%d) and cell data values (%d) mismatch." % (axialDist.GetNumberOfTuples(), jplcDataset.GetNumberOfCells)
+    assert jplcArray.GetNumberOfTuples() == jplcDataset.GetNumberOfCells(), "Number of cells (%d) and cell data values (%d) mismatch." % (jplcArray.GetNumberOfTuples(), jplcDataset.GetNumberOfCells())
+    
+    jplcDataset.GetCellData().AddArray(axialDist)
+    jplcDataset.GetCellData().AddArray(jplcArray)
+    
+    jplcMapWriter = vtk.vtkXMLPolyDataWriter()
+    jplcMapWriter.SetFileName("jplc.vtp")
+    jplcMapWriter.SetInput(jplcDataset)
+    jplcMapWriter.Update()
+    
+    # Provide a quick visualisation of the JPLC profile for validation.
+    pointsX = range(-int(axialDistRange[1] - 1), int(axialDistRange[1]))
+    pointsY = []
+    for pt in pointsX:
+        pointsY.append(sigmoidJPLC(pt))
+    
+    pyplot.plot(pointsX, pointsY, 'b')
+    pyplot.show()
 
 if __name__ == '__main__':
     print "Starting", os.path.basename(__file__)
